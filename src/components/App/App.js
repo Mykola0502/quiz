b1f664b782d9922673ae265cf319b2c026bd1cc1
Mykola Saliuk <haker0502@ukr.net>
2023-08-20 18:27:45 +0300

    налаштовує автоматичний деплой на GitHub Pages

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,13 @@
-import { Game } from "../Game";
-// import { Result } from "../Result";
+// import { Game } from "../Game";
+import { Result } from "../Result";
 
 import { Container } from "./App.styled";
 
 export const App = () => {
   return (
     <Container>
-      <Game />
-      {/* <Result /> */}
+      {/* <Game /> */}
+      <Result />
     </Container>
   );
 };
