948783c20dbd2e96b01362060a51799d4c578a74
Mykola Saliuk <haker0502@ukr.net>
2023-08-29 23:11:10 +0300

    додає відмінювання слова "Відповідь" у результатах

--- src/components/Result/Result.js
+++ src/components/Result/Result.js
@@ -2,11 +2,21 @@ import questions from "../../db/questions.json";
 import { Container, Button, Img } from "./Result.styled";
 
 export const Result = ({ correct }) => {
+  let answer = "";
+
+  if (correct === 1) {
+    answer = "відповідь";
+  } else if (correct === 2 || correct === 3 || correct === 4) {
+    answer = "відповіді";
+  } else {
+    answer = "відповідей";
+  }
+
   return (
     <Container>
       <Img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
       <h2>
-        Ви відгадали {correct} відповіді з {questions.length}
+        Ви відгадали {correct} {answer} з {questions.length}
       </h2>
       <a href="/">
         <Button>Спробувати знову</Button>
