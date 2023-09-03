ac9ff010a0f0c5f6343660d35e003804b0639663
Mykola Saliuk <haker0502@ukr.net>
2023-09-03 15:28:10 +0300

    додає адаптивність

--- src/components/App/App.styled.jsx
+++ src/components/App/App.styled.jsx
@@ -1,9 +1,19 @@
 import styled from "@emotion/styled";
 
 export const Container = styled.div`
-  width: 500px;
+  /* width: 500px; */
   border-radius: 30px;
   padding: 40px;
   background-color: #fff;
   position: relative;
+
+  @media (max-width: 767.98px) {
+    padding: 40px 10px;
+    width: 280px;
+  }
+
+  @media (min-width: 768px) {
+    padding: 40px;
+    width: 500px;
+  }
 `;
